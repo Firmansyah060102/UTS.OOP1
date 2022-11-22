16166f2b92645037df174cf4978daf2ba6ca12ab
Firmansyah <96497106+Firmansyah060102@users.noreply.github.com>
2022-11-21 21:46:40 -0800

    Add files via upload

---
+++ src/SepedaMotor.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author PCs Laptop New
+ */
+public class SepedaMotor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String merk;
+        String cc;
+        String mesin;
+        String kerangka;
+        
+        merk ="honda";
+        cc="150 cc";
+        mesin="343445";
+        kerangka="3402";
+        
+        System.out.println("Data Kendaraan sepeda motor");
+        System.out.println("merk :"+merk);
+        System.out.println("cc kendaraan :"+cc);
+        System.out.println("no mesin :"+mesin);
+        System.out.println("no kerangka :"+kerangka);
+        
+        
+        // TODO code application logic here
+    }
+    
+}
